22f437d9ba3fbdd34c8fffafbbf0989f43214b7b
Alfred SOSSA <alfredsossa17@gmail.com>
2025-11-09 02:34:20 +0100

    encore

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { motion } from 'framer-motion'
-import { useAuth } from '../../contexts/Authcontext'
+import { useAuth } from '../../contexts/AuthContext'
 
 const Header = () => {
   const location = useLocation()
